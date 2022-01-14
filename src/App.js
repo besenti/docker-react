f98b2565813f6ba64deffd2c86409d808bc91d6e
besenti <besenti.samson@gmail.com>
2022-01-14 11:52:07 +0200

    trivia update to trigger a build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello productions - served by nginx again!
+          Hello automated build pipeline.
         </p>
         <a
           className="App-link"
